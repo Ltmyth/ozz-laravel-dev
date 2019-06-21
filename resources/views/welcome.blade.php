37962f7a4f00823e5e71ffa1e8431e0b2b47e2ed
ltmyth <latimmark@gmail.com>
2019-06-21 17:56:59 +0300

    m t b

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,5 +13,66 @@
                 </h3>	
     		</div>
     	</div>
+
+        <div id="how" class="row contain">
+            <div class="row center" >
+                <strong><h3>How it Works</h3></strong>
+                <br>
+            </div>
+
+            <div class="row center">
+                <div class="col-md-4" >
+                    1
+                    <br>
+                    <h3 class="lt-al"><strong>Register & Login</strong></h3>
+                    <h4>
+                        <p class="lt-al">
+                            0zz register and access requirements;<br>
+                            <ul class="lt-al">
+                                <li>a valid email</li>
+                                <br>
+                                <li>a unique username</li>
+                                <br>
+                                <li>
+                                    a secure memorable <br>
+                                    password or access key
+                                </li>
+                            </ul>                             
+                        </p>
+                    </h4>
+                </div>
+                <div class="col-md-4" >
+                    2
+                    <br>
+                    <h3 class="lt-al"><strong>Setup & Topup</strong></h3>
+                    <h4>
+                        <p class="lt-al">
+                           0zz profile setup requirements; 
+                        </p>
+                        <ul class="lt-al">
+                            <li>Active 0zz account</li>
+                            <br>
+                            <li>0zz Wallet</li>
+                            <br>
+                            <li>Active Mobile Money Number</li>
+                        </ul>
+                    </h4>
+                </div>
+                <div class="col-md-4" >
+                    3
+                    <br>
+                    <h3 class="lt-al"><strong>Connect & Use</strong></h3>
+                    <h4>
+                        <p class="lt-al">Lastly!</p>
+                        <p class="lt-al">
+                            Search, find and connect with community-based users and services
+                            using your wallet to secure or pay for services quick and click 
+                            easy.
+                        </p>
+                    </h4>
+                </div>
+            </div>           
+        </div>
+        <br><br>
 	@endsection    
 @endguest
