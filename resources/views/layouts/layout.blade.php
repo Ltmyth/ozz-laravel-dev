a7d3298ebf1d8f286d14e7cc5dac15f9180e40d2
ltmyth <latimmark@gmail.com>
2019-12-05 19:24:00 +0300

    layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,7 +12,23 @@
             <!-- Site Title -->
             <title>theohz.com</title>
 
-            
+            <!-- scripts -->
+            <script src="js/vendor/jquery-2.2.4.min.js"></script>
+            <script src="js/popper.min.js"></script>
+            <script src="js/vendor/bootstrap.min.js"></script>      
+            <script src="js/easing.min.js"></script>            
+            <script src="js/hoverIntent.js"></script>
+            <script src="js/superfish.min.js"></script> 
+            <script src="js/jquery.ajaxchimp.min.js"></script>
+            <script src="js/jquery.magnific-popup.min.js"></script> 
+            <script src="js/jquery.tabs.min.js"></script>                       
+            <script src="js/jquery.nice-select.min.js"></script>    
+            <script src="js/isotope.pkgd.min.js"></script>          
+            <script src="js/waypoints.min.js"></script>
+            <script src="js/jquery.counterup.min.js"></script>
+            <script src="js/simple-skillbar.js"></script>                           
+            <script src="js/owl.carousel.min.js"></script>                          
+            <script src="js/mail-script.js"></script> 
 
             <!-- Fonts -->
             <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
@@ -250,24 +266,8 @@
               document.getElementById("content").style.display = "block";
             }
 
-        </script>        
-
-        <script src="js/vendor/jquery-2.2.4.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/vendor/bootstrap.min.js"></script>      
-        <script src="js/easing.min.js"></script>            
-        <script src="js/hoverIntent.js"></script>
-        <script src="js/superfish.min.js"></script> 
-        <script src="js/jquery.ajaxchimp.min.js"></script>
-        <script src="js/jquery.magnific-popup.min.js"></script> 
-        <script src="js/jquery.tabs.min.js"></script>                       
-        <script src="js/jquery.nice-select.min.js"></script>    
-        <script src="js/isotope.pkgd.min.js"></script>          
-        <script src="js/waypoints.min.js"></script>
-        <script src="js/jquery.counterup.min.js"></script>
-        <script src="js/simple-skillbar.js"></script>                           
-        <script src="js/owl.carousel.min.js"></script>                          
-        <script src="js/mail-script.js"></script>  
+        </script>   
+ 
 </html>
 
      
