33e615dedaedcafdbe82391e917901e6cfe6495e
ltmyth <latimmark@gmail.com>
2019-12-23 17:56:19 +0300

    clear comment migration error

---
+++ database/migrations/2019_12_18_184946_alter_comment_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->string('author')->change();
+            $table->string('liked_by')->change();           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
