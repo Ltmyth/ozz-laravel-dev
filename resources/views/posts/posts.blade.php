f9f0c3db0a47754c71a7c6ce364ed22081704efb
ltmyth <latimmark@gmail.com>
2019-12-28 14:40:23 +0300

    likes

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -74,7 +74,7 @@
 						<div id="modal" class="container mb-10">
 							<div class="row pb-20" >
 								<!-- like post -->
-								@if($post->likes <= 0)
+								@if($likes <= 0)
 									<form id="likeForm"  method="POST" action="/like" >
 										@csrf
 										<input type="hidden" name="post_id" value="{{$post_id}}">
@@ -87,7 +87,8 @@
 										<br>
 										<big class="container red">Like</big>
 									</form>	
-								@elseif($post->likes == 1)
+								@endif
+								@if($likes>=1)
 									<form id="unlikeForm" method="POST" action="/unlike/{{$post_id}}" >
 										@csrf
 										<input type="hidden"  value="$post_id">
@@ -99,20 +100,6 @@
 										</button>
 										<br>
 										<big class="container red">Like</big>
-									</form>														
-								@endif
-								@if($post->likes >= 2)
-									<form id="unlikeForm" method="POST" action="/unlike/{{$post_id}}" >
-										@csrf
-										<input type="hidden"  value="$post_id">
-										<input type="number" class="hidden" value="{{ Auth::user()->name }}">
-										<br>
-										<button  type="submit" 
-										id="like{{ $post->id }}" class="btn btn-sm btn-danger px100" onclick="javascript:document.getElementById('preloader').style.display='block';">
-											<i class="lnr lnr-heart"></i> <b class="white">{{ $likes}}</b>
-										</button>
-										<br>
-										<big class="container red">Likes</big>
 									</form>														
 								@endif
 								<!-- end like  -->
