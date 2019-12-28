e551ef367b6e04634450f4306e797729c10126a9
ltmyth <latimmark@gmail.com>
2019-12-28 15:25:38 +0300

    like

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
-use App\like;
+use App\Like;
 use Redirect,Response;
 
 class LikeController extends Controller
