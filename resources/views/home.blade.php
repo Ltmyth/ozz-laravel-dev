b67cf1253ed0b4101f90e84c0f69922253bfd017
ltmyth <latimmark@gmail.com>
2019-12-29 01:06:31 +0300

    minus upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,8 +67,8 @@
                                 <br>
                                 <div class="form-group row left-space" >
                                     &nbsp;&nbsp;
-                                   <big ><i class="lnr lnr-paperclip"></i></big><input type="button" name="upload"  value="Upload" onclick="getfile()"  id="myBtn">
-                                   &nbsp;&nbsp;
+                                   <!-- <big ><i class="lnr lnr-paperclip"></i></big><input type="button" name="upload"  value="Upload" onclick="getfile()"  id="myBtn">
+                                   &nbsp;&nbsp; -->
                                     <button type="submit" class="btn btn-lg btn-outline-primary">
                                         <i class="lnr lnr-cloud-upload">Post</i> 
                                     </button> 
