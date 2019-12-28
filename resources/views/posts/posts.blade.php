092c21f72abafe0446df1c248ae3510c7cfb7eb0
ltmyth <latimmark@gmail.com>
2019-12-28 15:00:37 +0300

    likes

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -74,21 +74,21 @@
 						<div id="modal" class="container mb-10">
 							<div class="row pb-20" >
 								<!-- like post -->
-								@if($likes <= 0)
+								@if($likes<=0)
 									<form id="likeForm"  method="POST" action="/like" >
 										@csrf
 										<input type="hidden" name="post_id" value="{{$post_id}}">
 										<input type="number" class="hidden" name="liked_by" value="{{ Auth::user()->name }}">
 										<br>
-										<button  
-										id="like{{ $post->id }}" type="submit" class="btn btn-sm btn-outline-danger px100" onclick="javascript:document.getElementById('preloader').style.display='block';">
+										<button type="submit"  
+										id="like{{ $post->id }}" class="btn btn-sm btn-outline-danger px100" onclick="javascript:document.getElementById('preloader').style.display='block';">
 											<i class="lnr lnr-heart"></i>
 										</button>
 										<br>
 										<big class="container red">Like</big>
 									</form>	
 								@endif
-								
+
 								@if($likes>=1)
 									<form id="unlikeForm" method="POST" action="/unlike/{{$post_id}}" >
 										@csrf
