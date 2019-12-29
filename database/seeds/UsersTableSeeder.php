ff264aaa381ad4c6d7b8a05abb176a456f7e9424
ltmyth <latimmark@gmail.com>
2019-12-29 13:26:59 +0300

    welcome message

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,10 +12,17 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
-         DB::table('users')->insert([
-            'name' => 'QQ',
-            'email' => 'nn@gmail.com',
-            'password' => bcrypt('12345678'),
+        DB::table('users')->insert([
+            'name' => 'Latim Mark',
+            'email' => 'latimmark@gmail.com',
+            'password' => bcrypt('@1nile5nation'),
+        ]);
+
+        DB::table('messages')->insert([
+            'message' => 'Hello! welcome to theohz platform',
+            'receiver' => 'everyone',
+            'author' => 'theohz chatbot',
+            'created_at' => now(),
         ]);
     }
 }
