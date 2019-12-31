4923993a8391b675744a63158ad0b2ba6dea7c0f
ltmyth <latimmark@gmail.com>
2019-12-31 14:56:54 +0300

    individual chat

--- resources/views/messages/individual_chat.blade.php
+++ resources/views/messages/individual_chat.blade.php
@@ -2,7 +2,7 @@
 	<hr>
         @if($chats)
             @foreach ($chats as $chat)
-                <div class="container">
+                <div class="row">
                     <div class="row light-bg tlbr-10 trbr-10 blbr-10 pt-20  pb-20 pull-left">
                         <p class="lt-al ml-20 pl-10 white">
                             <strong>
