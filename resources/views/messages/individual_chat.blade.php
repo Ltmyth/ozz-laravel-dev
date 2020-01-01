a1fc5aa7d8c911eefb7c79c24e8dbcaf7ee0f035
ltmyth <latimmark@gmail.com>
2020-01-01 16:26:23 +0300

    individual chat

--- resources/views/messages/individual_chat.blade.php
+++ resources/views/messages/individual_chat.blade.php
@@ -38,7 +38,7 @@
                                         </i>
                                     </small>
                                 </div>
-                                <div class="row light-bg col-lg-6 brbr-10 tlbr-10 blbr-10 pt-20  pb-20">
+                                <div class="row light-bg brbr-10 tlbr-10 blbr-10 pt-20  pb-20">
                                     <p class="lt-al ml-20 pl-10 white">
                                         <strong>
                                             {{ $chat->message }}
