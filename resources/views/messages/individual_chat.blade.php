1cbe021f5cfad1e748fb36b2c0987fa4cec624db
ltmyth <latimmark@gmail.com>
2020-01-01 12:07:34 +0300

    individual chat

--- resources/views/messages/individual_chat.blade.php
+++ resources/views/messages/individual_chat.blade.php
@@ -1,10 +1,11 @@
 @auth
 	@if($chats)
+        <hr>
+        <div class="row lt-al">
+        
         @foreach ($chats as $chat)
-            <hr>
-            <div class="row lt-al">
-                <strong class="blue">{{ $chat->author }}</strong>
-            </div>
+            <strong class="blue">{{ $chat->author }}</strong>
+            <br>
             <div class="row light-bg col-lg-6 tlbr-10 trbr-10 blbr-10 pt-20  pb-20 pull-left">
                 <p class="lt-al ml-20 pl-10 white">
                     <strong>
@@ -12,7 +13,8 @@
                     </strong>
                 </p>
             </div> 
-            <hr>              
+            <hr>   
+            </div>           
         @endforeach
     @endif		
 @endauth
