ca18f7e8a233de9fd8297509631bdec7e44cf842
ltmyth <latimmark@gmail.com>
2020-01-01 13:07:49 +0300

    individual chat

--- resources/views/messages/individual_chat.blade.php
+++ resources/views/messages/individual_chat.blade.php
@@ -4,7 +4,7 @@
             <hr>
             @foreach ($chats as $chat)
                 <div class="row">
-                    <div class="row col-lg-6 pull-left">
+                    <div class="row col-lg-8 pull-left">
                         @if($user)
                         <strong class="blue">
                             @if($chat->author == $user)
@@ -14,6 +14,12 @@
                             @endif
                         </strong>
                         @endif
+                        &nbsp;
+                        <small>
+                            <i class="black fz-15">
+                                {{ $chat->created_at->diffForHumans() }}
+                            </i>
+                        </small>
                     </div>
                     <div class="row light-bg col-lg-6 tlbr-10 trbr-10 blbr-10 pt-20  pb-20 pull-left">
                         <p class="lt-al ml-20 pl-10 white">
