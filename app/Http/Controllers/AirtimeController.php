088dfe0cef5f24e1c5b0aa955897e4ca6804e0ed
ltmyth <latimmark@gmail.com>
2020-01-06 21:22:23 +0300

    update 0.0.6

--- app/Http/Controllers/AirtimeController.php
+++ app/Http/Controllers/AirtimeController.php
@@ -55,7 +55,7 @@ class AirtimeController extends Controller
             return redirect('at-self')->with('error_message', $error_message);
         }
         elseif ($receiver != "0" && $user_balance>$cost){
-            if ($cost>0.01)) {
+            if($cost>0.01) {
                 $username = "Mat";
                 $apiKey = "4c2abe345bc83d4bcfb557a7bf75dc550e8138f77395f7f5611a032bcb5f6eda";
                 
@@ -118,7 +118,7 @@ class AirtimeController extends Controller
                     echo "Error: ".$e->getMessage();
                 }
                 return redirect('sent_airtime');  
-            }elseif ($cost<0.01)) {
+            }elseif ($cost<0.01) {
                 $error_message = "Request is below lower limit";
                 return redirect('at-self')->with('error_message', $error_message);
             }                         
