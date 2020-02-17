fa5adc7bfcf80e2312a82638375cf41d31e04e3a
ltmyth <latimmark@gmail.com>
2020-02-17 17:30:37 +0300

    update 1.1

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -246,7 +246,7 @@ class MessageController extends Controller
             // $data = array_slice($file, 1);
 
             $row = 1;
-            if (($handle = fopen($file, "r")) !== FALSE) {
+            if (($handle = fopen($receiverz, "r")) !== FALSE) {
                 while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     $num = count($data);
                     echo "<p> $num fields in line $row: <br /></p>\n";
