4b95980894e7565c86822ab278db2037951919eb
bonyta <bbonyta@gmail.com>
2020-03-13 14:18:13 +0300

    update wallet 1.1

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -75,7 +75,7 @@
                   <small class="red">
                     Payment Method
                   </small>  
-                  <select class="form-control col-lg-6">
+                  <select class="form-control col-lg-6 container">
                     <option>Mobile-Money</option>
                     <option disabled>Mastercard</option>
                   </select>                  
