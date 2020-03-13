7cafaca61595a73fecff85a66071a6b12c0697aa
bonyta <bbonyta@gmail.com>
2020-03-14 00:45:45 +0300

    update bulksms 1.15

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -263,7 +263,7 @@ class MessageController extends Controller
                         // while($data = $row) {
                         //     $result[$data[0]] = $data[1];
                         // }
-                        $sub = $row; 
+                        $sub = 1*$row; 
                         $phoneNumber = dd($sub);                                      
                     }
                     fclose($receiverz);
