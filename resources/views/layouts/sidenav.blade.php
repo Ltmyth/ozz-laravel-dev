aa68bae59b1ed9c039907a04bae995652f4e4611
bonyta <bbonyta@gmail.com>
2020-04-27 19:45:22 +0300

    new layout 1.12

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -9,7 +9,7 @@
             <button id="home_btn" class="btn btn-md btn-outline-warning blue-bg col-lg-2">
                 <span class="lnr lnr-users white"></span>
                 <strong class="white">  
-                    4You <!-- <span class="orange_dot"></span> -->
+                    In4Me <!-- <span class="orange_dot"></span> -->
                 </strong>
             </button>
         </a>
