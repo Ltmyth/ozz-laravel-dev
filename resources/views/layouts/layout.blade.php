d7f35fda7a4db81b654fb06b5eaf3e0d2ab9e9e3
bonyta <bbonyta@gmail.com>
2020-05-28 20:25:00 +0300

    2.0.5.0 input

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -30,11 +30,14 @@
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
             
 
-            <!-- include summernote css/js -->
+            <!-- include summernote  -->
+            <!-- include libraries(jQuery, bootstrap) -->
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+            <!-- include summernote css/js -->
             <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-
 
             <!-- styles -->
             <link rel="stylesheet" href="/css/linearicons.css">
