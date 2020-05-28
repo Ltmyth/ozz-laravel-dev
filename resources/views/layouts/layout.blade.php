a883e5cceda17cdc4b0b5f25b01018649339d505
bonyta <bbonyta@gmail.com>
2020-05-28 20:00:52 +0300

    2.0.4.8 input

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -39,6 +39,9 @@
             <!-- include summernote css/js -->
             <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
             <!-- styles -->
             <link rel="stylesheet" href="/css/linearicons.css">
